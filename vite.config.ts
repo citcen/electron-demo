6e2bf3437df5c513ff66a2dcdf3b02b03f5f38ed
citcen <citkecen@gmail.com>
2023-03-10 21:01:00 +0800

    ci: element-plus/icon vite集成按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,61 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+// 自动导入element-plus
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Icons from "unplugin-icons/dist/vite";
+import IconsResolver from "unplugin-icons/dist/resolver";
 
 export default defineConfig(() => {
   return {
     plugins: [
       vue(),
+      AutoImport({
+        // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        resolvers: [
+          ElementPlusResolver(),
+          // Auto import icon components
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: "Icon",
+          }),
+        ],
+      }),
+
+      Components({
+        resolvers: [
+          // Auto register icon components
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ["ep"],
+          }),
+          // Auto register Element Plus components
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+        ],
+      }),
+
+      Icons({
+        autoInstall: true,
+      }),
     ],
     resolve: {
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.less', '.css'],
+      extensions: [
+        ".mjs",
+        ".js",
+        ".ts",
+        ".jsx",
+        ".tsx",
+        ".json",
+        ".less",
+        ".css",
+      ],
       alias: {
-        "/@": resolve(__dirname, './src')
-      }
+        "/@": resolve(__dirname, "./src"),
+      },
     },
-  }
-})
+  };
+});
